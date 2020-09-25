aedcd6a67a2079ecdd78f5d830a7e5b5ad8037a9
dbc2201 <divyanshb22@gmail.com>
2020-09-25 22:44:31 +0530

    add an intent to open SecondActivity

--- app/src/main/java/dbc/intentsdemo/MainActivity.kt
+++ app/src/main/java/dbc/intentsdemo/MainActivity.kt
@@ -1,5 +1,6 @@
 package dbc.intentsdemo
 
+import android.content.Intent
 import android.os.Bundle
 import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
@@ -10,7 +11,10 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
         val button: Button = findViewById(R.id.button)
         button.setOnClickListener {
-
+            val newActivityIntent: Intent = Intent(
+                    this,
+                    SecondActivity::class.java
+            )
         }
     }
 }

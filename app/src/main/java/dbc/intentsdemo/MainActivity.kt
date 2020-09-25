330cff62a2e3bfca2feb55e01c47d0979615a471
dbc2201 <divyanshb22@gmail.com>
2020-09-25 22:43:30 +0530

    add a new onClick listener for the button

--- app/src/main/java/dbc/intentsdemo/MainActivity.kt
+++ app/src/main/java/dbc/intentsdemo/MainActivity.kt
@@ -9,5 +9,8 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         val button: Button = findViewById(R.id.button)
+        button.setOnClickListener {
+
+        }
     }
 }
